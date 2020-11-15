ae925085cfc258ebd4fdd30d27257f589aeae405
PeganovAnton <peganoff2@mail.ru>
2020-11-15 12:47:10 -0800

    Add yttm model to dataset
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/conf/ngc_commands/debug_attention_is_all_you_need_en_de.sh
+++ examples/nlp/machine_translation/conf/ngc_commands/debug_attention_is_all_you_need_en_de.sh
@@ -4,15 +4,11 @@ pip install -r requirements/requirements.txt \
   && export HYDRA_FULL_ERROR=1 \
   && echo "NeMo path: ${nemo_path}" \
   && export PYTHONPATH="${nemo_path}" \
-  && cd /data \
-  && cat train.en train.de > yttm_train.ende \
-  && echo "current path when creating yttm model: $(pwd)" \
-  && yttm bpe --data yttm_train.ende --model bpe_37k_en_de_yttm.model --vocab_size 37000 \
-  && mkdir -p wmt14_en_de2 \
-  && cd wmt14_en_de2 \
-  && cp ../bpe_37k_en_de_yttm.model ./ \
-  && cp ../test* ./ \
-  && cp ../valid* ./ \
+  && mkdir -p /wmt14_en_de2 \
+  && cd /wmt14_en_de2 \
+  && cp /data/bpe_37k_en_de_yttm.model ./ \
+  && cp /data/test* ./ \
+  && cp /data/valid* ./ \
   && cp valid.en train.en \
   && cp valid.de train.de \
   && cd  "${nemo_path}/examples/nlp/machine_translation" \
