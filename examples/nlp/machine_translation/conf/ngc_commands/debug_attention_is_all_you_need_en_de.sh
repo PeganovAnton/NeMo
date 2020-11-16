a9d859149b2725cae56d3785b86e115a2e4304a9
PeganovAnton <peganoff2@mail.ru>
2020-11-15 23:30:02 -0800

    Improve conf and command
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/conf/ngc_commands/debug_attention_is_all_you_need_en_de.sh
+++ examples/nlp/machine_translation/conf/ngc_commands/debug_attention_is_all_you_need_en_de.sh
@@ -2,13 +2,6 @@ pip install -r export nemo_path=$(pwd) \
   && export HYDRA_FULL_ERROR=1 \
   && echo "NeMo path: ${nemo_path}" \
   && export PYTHONPATH="${nemo_path}" \
-  && mkdir -p /wmt14_en_de2 \
-  && cd /wmt14_en_de2 \
-  && cp /data/bpe_37k_en_de_yttm.model ./ \
-  && cp /data/test* ./ \
-  && cp /data/valid* ./ \
-  && cp valid.en train.en \
-  && cp valid.de train.de \
   && cd  "${nemo_path}/examples/nlp/machine_translation" \
   && python train.py -cn debug_on_ngc \
   && python test.py model.test_checkpoint_path=best.ckpt -cn debug_on_ngc
