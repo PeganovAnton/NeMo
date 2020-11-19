15474a4a5c6c0e1664e5b6cb80bccbbe0a69d419
Anton Peganov <apeganov@nvidia.com>
2020-11-20 02:53:16 +0300

    Add fp32 ngc debug scripts
    Signed-off-by: Anton Peganov <apeganov@nvidia.com>

---
+++ examples/nlp/machine_translation/conf/ngc_commands/debug_attention_is_all_you_need_en_de_fp32.sh
@@ -0,0 +1,10 @@
+pip install -r requirements/requirements.txt \
+  && pip install -r requirements/requirements_nlp.txt \
+  && export nemo_path=$(pwd) \
+  && export HYDRA_FULL_ERROR=1 \
+  && echo "NeMo path: ${nemo_path}" \
+  && export PYTHONPATH="${nemo_path}" \
+  && cd  "${nemo_path}/examples/nlp/machine_translation" \
+  && python train.py -cn debug_on_ngc_fp32 \
+  && python test.py model.test_checkpoint_path=best.ckpt -cn debug_on_ngc_fp32
+
