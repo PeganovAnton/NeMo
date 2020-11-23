031182d48f0379319eb54feea4d44dde81ce7d37
PeganovAnton <peganoff2@mail.ru>
2020-11-23 12:29:04 +0300

    Add small 1 gpu example
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/nlp/machine_translation/conf/wmt16/ngc_commands/attention_is_all_you_need_en_de_fp32_small_1gpu.sh
@@ -0,0 +1,24 @@
+pip install -r requirements/requirements.txt \
+  && pip install -r requirements/requirements_nlp.txt \
+  && export nemo_path=$(pwd) \
+  && export HYDRA_FULL_ERROR=1 \
+  && echo "NeMo path: ${nemo_path}" \
+  && export PYTHONPATH="${nemo_path}" \
+  && cd  "${nemo_path}/examples/nlp/machine_translation" \
+  && yttm bpe --data /data/train.clean.en-de.shuffled.common --model bpe_16k_en_de_yttm.model --vocab_size 16000 \
+  && ls /data \
+  && python train.py -cn wmt16/en_de_8gpu \
+      trainer.precision=32 \
+      model.train_ds.tokens_in_batch=1024 \
+      model.validation_ds.tokens_in_batch=256 \
+      model.test_ds.tokens_in_batch=256 \
+      model.machine_translation.num_layers=1 \
+      trainer.gpus=1 \
+  && python test.py -cn wmt16/en_de_8gpu \
+      trainer.precision=32 \
+      model.train_ds.tokens_in_batch=1024 \
+      model.validation_ds.tokens_in_batch=256 \
+      model.test_ds.tokens_in_batch=256 \
+      model.machine_translation.num_layers=1 \
+      trainer.gpus=1
+
