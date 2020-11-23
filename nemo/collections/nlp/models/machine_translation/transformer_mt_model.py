143db36e5078f7ccbf6d5954a7e960bbc4dad856
PeganovAnton <peganoff2@mail.ru>
2020-11-23 09:16:07 +0300

    Sketch fix of src mask issue
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- nemo/collections/nlp/models/machine_translation/transformer_mt_model.py
+++ nemo/collections/nlp/models/machine_translation/transformer_mt_model.py
@@ -228,6 +228,8 @@ class TransformerMTModel(ModelPT):
         """
         bsz = src.shape[0]
         mem_tokens = src.new_tensor(np.full((bsz, self.num_mem_tokens), self.src_tokenizer.mem_id))
+        mem_mask = src_mask.new_tensor(np.full((bsz, self.num_mem_tokens), False))
+        src_mask = torch.cat((mem_mask, src_mask), dim=1)
         src = torch.cat((mem_tokens, src), dim=1)
         src_embeddings = self.src_embedding_layer(input_ids=src)
         # src_embeddings *= src_embeddings.new_tensor(self.emb_scale)
