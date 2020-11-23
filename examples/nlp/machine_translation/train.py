548480c04a7cd8d039bb04ed5b01058b4754fc23
PeganovAnton <peganoff2@mail.ru>
2020-11-23 18:09:54 +0300

    print ptl vrs
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/train.py
+++ examples/nlp/machine_translation/train.py
@@ -16,6 +16,7 @@
 import os
 
 import pytorch_lightning as pl
+print(pl.__version__)
 from omegaconf import DictConfig
 
 from nemo.collections.nlp.models.machine_translation import TransformerMTModel
