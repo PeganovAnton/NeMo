ca676076935f4fdc35cd24d74310f30a4de1121d
PeganovAnton <peganoff2@mail.ru>
2020-12-01 16:45:04 +0300

    reduce num epchs
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/conf/wmt16/circe_commands/restore_attention_is_all_you_need_de_en.sh
+++ examples/nlp/machine_translation/conf/wmt16/circe_commands/restore_attention_is_all_you_need_de_en.sh
@@ -1,14 +1,16 @@
 pip install -r requirements/requirements.txt \
   && pip install -r requirements/requirements_nlp.txt \
+  && pip install webdataset \
+  && pip install transformers==3.5.0 \
   && export nemo_path=$(pwd) \
   && export HYDRA_FULL_ERROR=1 \
   && echo "NeMo path: ${nemo_path}" \
   && export PYTHONPATH="${nemo_path}" \
   && cd  "${nemo_path}/examples/nlp/machine_translation" \
-  && data_path=/workspace/mydatasets/sandeepsub/wmt18 \
-  && bi_path=${data_path}/parallel \
-  && mono_en_path=${data_path}/wmt18_en_mono \
-  && mono_de_path=${data_path}/wmt18_de_mono \
+  && export mono_data_path=/workspace/mydatasets/sandeepsub/wmt18 \
+  && export bi_path=/workspace/mydatasets/sandeepsub/wmt/wmt18_en_de/parallel \
+  && export mono_en_path=${mono_data_path}/wmt18_en_mono \
+  && export mono_de_path=${mono_data_path}/wmt18_de_mono \
   && cat ${bi_path}/train.clean.* ${mono_en_path}/monolingual.2* ${mono_de_path}/monolingual.2* > all_text.txt \
   && yttm bpe --data all_text.txt --model bpe_16k_en_de_yttm.model --vocab_size 16000 \
   && python train.py -cn wmt16/de_en_8gpu \
@@ -21,7 +23,7 @@ pip install -r requirements/requirements.txt \
       model.test_ds.src_file_name=${bi_path}/wmt14-de-en.src \
       model.test_ds.tgt_file_name=${bi_path}/wmt14-de-en.ref \
       exp_manager.exp_dir=/workspace/result \
-      +exp_manager.explicit_log_dir=/workspace/result/TransformerMT/2020-11-24_11-04-17 \
+      +exp_manager.explicit_log_dir=/workspace/result/TransformerMT/2020-12-01_04-53-17 \
       +exp_manager.resume_if_exists=true \
       trainer.max_epochs=50 \
   && python test.py -cn wmt16/de_en_8gpu \
