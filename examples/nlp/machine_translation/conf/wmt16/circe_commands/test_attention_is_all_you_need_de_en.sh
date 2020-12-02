dd3b0233668610d5e7efa219fc2e25380e692c05
PeganovAnton <peganoff2@mail.ru>
2020-12-02 13:03:58 +0300

    Make minor fixes
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/nlp/machine_translation/conf/wmt16/circe_commands/test_attention_is_all_you_need_de_en.sh
@@ -0,0 +1,24 @@
+pip install -r requirements/requirements.txt \
+  && pip install -r requirements/requirements_nlp.txt \
+  && pip install webdataset \
+  && pip install transformers==3.5.0 \
+  && export nemo_path=$(pwd) \
+  && export HYDRA_FULL_ERROR=1 \
+  && echo "NeMo path: ${nemo_path}" \
+  && export PYTHONPATH="${nemo_path}" \
+  && cd  "${nemo_path}/examples/nlp/machine_translation" \
+  && export mono_data_path=/workspace/mydatasets/sandeepsub/wmt18 \
+  && export bi_path=/workspace/mydatasets/sandeepsub/wmt/wmt18_en_de/parallel \
+  && export mono_en_path=${mono_data_path}/wmt18_en_mono \
+  && export mono_de_path=${mono_data_path}/wmt18_de_mono \
+  && cat ${bi_path}/train.clean.* ${mono_en_path}/monolingual.2* ${mono_de_path}/monolingual.2* > all_text.txt \
+  && yttm bpe --data all_text.txt --model bpe_16k_en_de_yttm.model --vocab_size 16000 \
+  && python test.py -cn wmt16/de_en_8gpu \
+      trainer.gpus=16 \
+      model.train_ds.src_file_name=${bi_path}/train.clean.de \
+      model.train_ds.tgt_file_name=${bi_path}/train.clean.en \
+      model.validation_ds.src_file_name=${bi_path}/wmt13-de-en.src \
+      model.validation_ds.tgt_file_name=${bi_path}/wmt13-de-en.ref \
+      model.test_ds.src_file_name=${bi_path}/wmt14-de-en.src \
+      model.test_ds.tgt_file_name=${bi_path}/wmt14-de-en.ref \
+      exp_manager.exp_dir=/workspace/result
