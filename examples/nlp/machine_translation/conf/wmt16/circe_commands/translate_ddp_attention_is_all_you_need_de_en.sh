36f9d8918ff604ac0874b6873e6695468367af70
PeganovAnton <peganoff2@mail.ru>
2020-12-03 17:16:27 +0300

    Add de en translate script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/conf/wmt16/circe_commands/translate_ddp_attention_is_all_you_need_de_en.sh
+++ examples/nlp/machine_translation/conf/wmt16/circe_commands/translate_ddp_attention_is_all_you_need_de_en.sh
@@ -11,14 +11,14 @@ pip install -r requirements/requirements.txt \
   && export bi_path=/workspace/mydatasets/apeganov/wmt/wmt18_en_de/parallel \
   && export mono_en_path=${mono_data_path}/wmt18_en_mono \
   && export mono_de_path=${mono_data_path}/wmt18_de_mono \
-  && export train_results_path=/workspace/old_results/result_en_de \
-  && export result_dir=${train_results_path}/mono_en_translation \
+  && export train_results_path=/workspace/old_results/result_de_en \
+  && export result_dir=${train_results_path}/mono_de_translation \
   && export best_ckpt=${train_results_path}/best.ckpt \
   && export yttm_model=bpe_16k_en_de_yttm.model \
   && if [ ! -f ${yttm_model} ]; then cat ${bi_path}/train.clean.en ${bi_path}/train.clean.de ${mono_en_path}/monolingual.25000000.en ${mono_de_path}/monolingual.25000000.de > all_text.txt && yttm bpe --data all_text.txt --model bpe_16k_en_de_yttm.model --vocab_size 16000; fi \
   && python translate_ddp.py \
       --model ${best_ckpt} \
-      --text2translate ${mono_en_path}/monolingual.25000000.en \
+      --text2translate ${mono_en_path}/monolingual.25000000.de \
       --tokenizer_model bpe_16k_en_de_yttm.model \
       --max_num_tokens_in_batch 4000 \
       --result_dir ${result_dir}
