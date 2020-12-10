d09887781841b6061d3409b8a9f137596a031984
PeganovAnton <peganoff2@mail.ru>
2020-12-10 10:14:03 +0300

    Add checking of checkpoint existence
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/wmt18_with_back_translation/circe_commands/4_stages_post_traning/debug_attention_is_all_you_need_de_en.sh
+++ examples/nlp/machine_translation/wmt18_with_back_translation/circe_commands/4_stages_post_traning/debug_attention_is_all_you_need_de_en.sh
@@ -14,44 +14,64 @@ pip install -r requirements/requirements.txt \
   && export result_dir=nemo_experiments/debug_back_translation \
   && export stage1_dir=${result_dir}/stage1 \
   && if [ ! -f ${stage1_dir}/best.ckpt ]; then
-      python train.py -cn ${base_conf} \
+      if compgen -G ${stage1_dir}/TransformerMT/*/checkpoints/* > dev/null; then
+        export resume=true
+      else
+        export resume=false
+      fi \
+      && python train.py -cn ${base_conf} \
           trainer.gpus=${ngpus} \
           exp_manager.exp_dir=${stage1_dir} \
           trainer.max_epochs=${max_epochs} \
           trainer.max_steps=3 \
-          +exp_manager.resume_if_exists=true
+          +exp_manager.resume_if_exists=${resume}
      fi \
   && export stage2_dir=${result_dir}/stage2 \
   && if [ ! -f ${stage2_dir}/best.ckpt ]; then
-      python train.py -cn ${base_conf} \
+      if compgen -G ${stage2_dir}/TransformerMT/*/checkpoints/* > dev/null; then
+        export resume=true
+      else
+        export resume=false
+      fi \
+      && python train.py -cn ${base_conf} \
           trainer.gpus=${ngpus} \
           model.train_ds.tokens_in_batch=${train_n_tokens_in_batch} \
           exp_manager.exp_dir=${stage2_dir} \
-          +exp_manager.resume_if_exists=true \
+          +exp_manager.resume_if_exists=${resume} \
           trainer.max_epochs=${max_epochs} \
           trainer.max_steps=4 \
           +model.weights_checkpoint=${stage1_dir}/best.ckpt
      fi \
   && export stage3_dir=${result_dir}/stage3 \
   && if [ ! -f ${stage3_dir}/best.ckpt ]; then
-      python train.py -cn ${base_conf} \
+      if compgen -G ${stage3_dir}/TransformerMT/*/checkpoints/* > dev/null; then
+        export resume=true
+      else
+        export resume=false
+      fi \
+      && python train.py -cn ${base_conf} \
           trainer.gpus=${ngpus} \
           model.train_ds.tokens_in_batch=${train_n_tokens_in_batch} \
           exp_manager.exp_dir=${stage3_dir} \
           trainer.max_epochs=${max_epochs} \
           trainer.max_steps=5 \
-          +exp_manager.resume_if_exists=true \
+          +exp_manager.resume_if_exists=${resume} \
           +model.weights_checkpoint=${stage2_dir}/best.ckpt
      fi \
   && export stage4_dir=${result_dir}/stage4 \
   && if [ ! -f ${stage4_dir}/best.ckpt ]; then
-      python train.py -cn ${base_conf} \
+      if compgen -G ${stage4_dir}/TransformerMT/*/checkpoints/* > dev/null; then
+        export resume=true
+      else
+        export resume=false
+      fi \
+      && python train.py -cn ${base_conf} \
           trainer.gpus=${ngpus} \
           model.train_ds.tokens_in_batch=${train_n_tokens_in_batch} \
           exp_manager.exp_dir=${stage4_dir} \
           trainer.max_epochs=${max_epochs} \
           trainer.max_steps=6 \
-          +exp_manager.resume_if_exists=true \
+          +exp_manager.resume_if_exists=${resume} \
           +model.weights_checkpoint=${stage3_dir}/best.ckpt
      fi \
   && python test.py -cn ${base_conf} \
