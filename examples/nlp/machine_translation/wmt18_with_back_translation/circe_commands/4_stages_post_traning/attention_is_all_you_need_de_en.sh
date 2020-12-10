db777711bfdb9ab0fafb494004d42baeb4f71219
PeganovAnton <peganoff2@mail.ru>
2020-12-10 12:09:52 +0300

    Add quotes to command
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/wmt18_with_back_translation/circe_commands/4_stages_post_traning/attention_is_all_you_need_de_en.sh
+++ examples/nlp/machine_translation/wmt18_with_back_translation/circe_commands/4_stages_post_traning/attention_is_all_you_need_de_en.sh
@@ -22,8 +22,8 @@ pip3 install -r requirements/requirements.txt \
   && export stage1_dir=${result_dir}/stage1 \
   && mkdir -p ${stage1_dir} \
   && if [ ! -f ${stage1_dir}/best.ckpt ]; then
-      cat ${bi_path}/train.clean.de ${mono_data_path}/3M_en_mono_partiaally_translated_06.12.20/translations.txt > ${stage1_dir}/src.de \
-      && cat ${bi_path}/train.clean.en ${mono_data_path}/3M_en_mono_partiaally_translated_06.12.20/originals.txt > ${stage1_dir}/tgt.en \
+      cat ${bi_path}/train.clean.de ${mono_data_path}/3M_en_mono_partiaally_translated_06.12.20/translations.txt >"${stage1_dir}/src.de" \
+      && cat ${bi_path}/train.clean.en ${mono_data_path}/3M_en_mono_partiaally_translated_06.12.20/originals.txt >"${stage1_dir}/tgt.en" \
       && if compgen -G ${stage1_dir}/TransformerMT/*/checkpoints/* > dev/null; then
         export resume=true
       else
@@ -47,8 +47,8 @@ pip3 install -r requirements/requirements.txt \
   && export stage2_dir=${result_dir}/stage2 \
   && mkdir -p ${stage2_dir} \
   && if [ ! -f ${stage2_dir}/best.ckpt ]; then
-      cat ${bi_path}/train.clean.de ${mono_data_path}/6M_en_mono_partiaally_translated_06.12.20/translations.txt > ${stage2_dir}/src.de \
-      && cat ${bi_path}/train.clean.en ${mono_data_path}/6M_en_mono_partiaally_translated_06.12.20/originals.txt > ${stage2_dir}/tgt.en \
+      cat ${bi_path}/train.clean.de ${mono_data_path}/6M_en_mono_partiaally_translated_06.12.20/translations.txt >"${stage2_dir}/src.de" \
+      && cat ${bi_path}/train.clean.en ${mono_data_path}/6M_en_mono_partiaally_translated_06.12.20/originals.txt >"${stage2_dir}/tgt.en" \
       && if compgen -G ${stage2_dir}/TransformerMT/*/checkpoints/* > dev/null; then
         export resume=true
       else
@@ -72,8 +72,8 @@ pip3 install -r requirements/requirements.txt \
   && export stage3_dir=${result_dir}/stage3 \
   && mkdir -p ${stage3_dir} \
   && if [ ! -f ${stage3_dir}/best.ckpt ]; then
-      cat ${bi_path}/train.clean.de ${mono_data_path}/12M_en_mono_partiaally_translated_06.12.20/translations.txt > ${stage3_dir}/src.de \
-      && cat ${bi_path}/train.clean.en ${mono_data_path}/12M_en_mono_partiaally_translated_06.12.20/originals.txt > ${stage3_dir}/tgt.en \
+      cat ${bi_path}/train.clean.de ${mono_data_path}/12M_en_mono_partiaally_translated_06.12.20/translations.txt >"${stage3_dir}/src.de" \
+      && cat ${bi_path}/train.clean.en ${mono_data_path}/12M_en_mono_partiaally_translated_06.12.20/originals.txt >"${stage3_dir}/tgt.en" \
       && if compgen -G ${stage3_dir}/TransformerMT/*/checkpoints/* > dev/null; then
         export resume=true
       else
@@ -97,8 +97,8 @@ pip3 install -r requirements/requirements.txt \
   && export stage4_dir=${result_dir}/stage4 \
   && mkdir -p ${stage4_dir} \
   && if [ ! -f ${stage4_dir}/best.ckpt ]; then
-      cat ${bi_path}/train.clean.de ${mono_data_path}/24M_en_mono_partiaally_translated_06.12.20/translations.txt > ${stage4_dir}/src.de \
-      && cat ${bi_path}/train.clean.en ${mono_data_path}/24M_en_mono_partiaally_translated_06.12.20/originals.txt > ${stage4_dir}/tgt.en \
+      cat ${bi_path}/train.clean.de ${mono_data_path}/24M_en_mono_partiaally_translated_06.12.20/translations.txt >"${stage4_dir}/src.de" \
+      && cat ${bi_path}/train.clean.en ${mono_data_path}/24M_en_mono_partiaally_translated_06.12.20/originals.txt >"${stage4_dir}/tgt.en" \
       && if compgen -G ${stage4_dir}/TransformerMT/*/checkpoints/* > dev/null; then
         export resume=true
       else
