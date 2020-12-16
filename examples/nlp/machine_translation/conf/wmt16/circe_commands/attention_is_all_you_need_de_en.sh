1161a0e12da573622710b18418ff3685f0dedc08
PeganovAnton <peganoff2@mail.ru>
2020-12-17 00:20:19 +0300

    Make separate data for mem tokens experiment
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/conf/wmt16/circe_commands/attention_is_all_you_need_de_en.sh
+++ examples/nlp/machine_translation/conf/wmt16/circe_commands/attention_is_all_you_need_de_en.sh
@@ -8,14 +8,19 @@ pip3 install -r requirements/requirements.txt \
   && export PYTHONPATH="${nemo_path}" \
   && cd  "${nemo_path}/examples/nlp/machine_translation" \
   && export data_path=/workspace/mydatasets/apeganov/wmt18 \
-  && export bi_path=${data_path}/parallel_clean \
+  && export workdir=/workspace/mem_tokens_2nd_trial_compare \
+  && mkdir -p ${workdir}/data \
+  && export tok_model_name=bpe_35k_en_de_yttm.model \
+  && cp ${data_path}/${tok_model_name} ${workdir}/data/ \
+  && cp ${data_path}/parallel_clean ${workdir}/data/ \
+  && export bi_path=${workdir}/data/parallel_clean \
   && export valid_src=${bi_path}/newstest2013-de-en.src \
   && export valid_ref=${bi_path}/newstest2013-de-en.ref \
   && export test_src=${bi_path}/newstest2014-de-en.src \
   && export test_ref=${bi_path}/newstest2014-de-en.ref \
   && export par_train_src=${bi_path}/train.clean.filter.de.shuffled \
   && export par_train_ref=${bi_path}/train.clean.filter.en.shuffled \
-  && export tok_model=${data_path}/bpe_35k_en_de_yttm.model \
+  && export tok_model=${workdir}/data/${tok_model_name}\
   && python3 train.py -cn wmt16/de_en_8gpu \
       trainer.gpus=16 \
       model.train_ds.tokens_in_batch=10000 \
