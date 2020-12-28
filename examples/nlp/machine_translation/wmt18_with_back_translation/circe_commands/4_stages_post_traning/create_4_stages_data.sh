018835e67d8fdbc8da7a2184446f96b8aede4a8a
PeganovAnton <peganoff2@mail.ru>
2020-12-28 15:19:26 +0300

    Update create data script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/wmt18_with_back_translation/circe_commands/4_stages_post_traning/create_4_stages_data.sh
+++ examples/nlp/machine_translation/wmt18_with_back_translation/circe_commands/4_stages_post_traning/create_4_stages_data.sh
@@ -16,14 +16,26 @@ done
 for i in 0 1 2 3 4
 do
   echo "Composing phase ${i} train"
-  orinals_fn="stage${i}/originals.txt"
+  stage_dir="stage${i}"
+  mkdir -p "${stage_dir}"
+  originals_fn="${stage_dir}/originals.txt"
+  echo "Creating file ${originals_fn}"
+  echo "Appending parallel data from ${par_train_src}"
   cat "${par_train_src}" > "${originals_fn}"
   if [ -n "${stage_mono_sources[i]}" ]; then
+    echo "Appending mono data from ${stage_mono_sources[i]}"
     cat "${stage_mono_sources[i]}" >> "${originals_fn}"
   fi
-  translations_fn="stage${i}/translations.txt"
+  echo "Shuffling file ${originals_fn}"
+  python3 ~/PeganovNeMo/utils/shuffle_pairs.py "${originals_fn}"
+  translations_fn="${stage_dir}/translations.txt"
+  echo "Creating file ${translations_fn}"
+  echo "Appending parallel data from ${par_train_ref}"
   cat "${par_train_ref}" > "${translations_fn}"
   if [ -n "${stage_mono_refs[i]}" ]; then
+    echo "Appending mono lingual data from ${stage_mono_refs[i]}"
     cat "${stage_mono_refs[i]}" >> "${translations_fn}"
   fi
+  echo "Shuffling file ${translations_fn}"
+  python3 ~/PeganovNeMo/utils/shuffle_pairs.py "${translations_fn}"
 done
