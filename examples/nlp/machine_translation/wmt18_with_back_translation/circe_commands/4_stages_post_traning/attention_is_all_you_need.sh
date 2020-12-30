ec4f9633f9b9ad8148c906dcc73982a85c79beb1
PeganovAnton <peganoff2@mail.ru>
2020-12-30 17:10:43 +0300

    Add slurm command
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/wmt18_with_back_translation/circe_commands/4_stages_post_traning/attention_is_all_you_need.sh
+++ examples/nlp/machine_translation/wmt18_with_back_translation/circe_commands/4_stages_post_traning/attention_is_all_you_need.sh
@@ -1,24 +1,20 @@
-export stage1_dir=${result_dir}/stage1 \
-  && mkdir -p ${stage1_dir} \
+stage0_dir=${result_dir}/stage0 \
+  && mkdir -p ${stage0_dir} \
   && if [ "$1" -eq "0" ]; then
     echo "********STARTING PHASE 0********" \
-    export stage0_dir=${result_dir}/stage0 \
-    && mkdir -p ${stage0_dir} \
     && if [ ! -f ${stage0_dir}/best.ckpt ]; then
-      cat ${par_train_src} >"${stage0_dir}/originals.txt" \
-      && cat ${par_train_ref} >"${stage0_dir}/translations.txt" \
-      && python3 ${nemo_path}/utils/shuffle_pairs.py ${stage0_dir} \
-      && if compgen -G ${stage0_dir}/TransformerMT/*/checkpoints/* > /dev/null; then
-        export resume=true
+      if compgen -G ${stage0_dir}/TransformerMT/*/checkpoints/* > /dev/null; then
+        resume=true
       else
-        export resume=false
+        resume=false
       fi \
+      && stage_train_data="${data_path}/stage0" \
       && python3 train.py -cn ${base_conf} \
           trainer.num_nodes=${num_nodes} \
           trainer.gpus=${ngpus} \
           model.train_ds.tokens_in_batch=${train_n_tokens_in_batch} \
-          model.train_ds.src_file_name=${stage0_dir}/originals.txt \
-          model.train_ds.tgt_file_name=${stage0_dir}/translations.txt \
+          model.train_ds.src_file_name=${stage_train_data}/originals.txt \
+          model.train_ds.tgt_file_name=${stage_train_data}/translations.txt \
           model.validation_ds.src_file_name=${valid_src} \
           model.validation_ds.tgt_file_name=${valid_ref} \
           model.test_ds.src_file_name=${test_src} \
@@ -27,30 +23,29 @@ export stage1_dir=${result_dir}/stage1 \
           exp_manager.exp_dir=${stage0_dir} \
           trainer.max_epochs=${max_epochs} \
           +trainer.max_steps=30000 \
-          +exp_manager.resume_if_exists=${resume}
+          +exp_manager.resume_if_exists=${resume} \
+          model.optim.lr=${stage0_lr}
     fi \
-    && export stage1_weights=${stage0_dir}/best.ckpt \
+    && stage1_weights=${stage0_dir}/best.ckpt \
     && echo "********FINISHED PHASE 0********"
   else
-    export stage1_weights="$1"
+    stage1_weights="$1"
   fi \
-  && export stage1_dir=${result_dir}/stage1 \
+  && stage1_dir=${result_dir}/stage1 \
   && mkdir -p ${stage1_dir} \
   && if [ ! -f ${stage1_dir}/best.ckpt ]; then
-    cat ${par_train_src} ${mono_data_path}/3M/translations.txt >"${stage1_dir}/originals.txt" \
-    && cat ${par_train_ref} ${mono_data_path}/3M/originals.txt >"${stage1_dir}/translations.txt" \
-    && python3 ${nemo_path}/utils/shuffle_pairs.py ${stage1_dir} \
     && if compgen -G ${stage1_dir}/TransformerMT/*/checkpoints/* > /dev/null; then
-      export resume=true
+      resume=true
     else
-      export resume=false
+      resume=false
     fi \
+    && stage_train_data="${data_path}/stage1" \
     && python3 train.py -cn ${base_conf} \
         trainer.num_nodes=${num_nodes} \
         trainer.gpus=${ngpus} \
         model.train_ds.tokens_in_batch=${train_n_tokens_in_batch} \
-        model.train_ds.src_file_name=${stage1_dir}/originals.txt \
-        model.train_ds.tgt_file_name=${stage1_dir}/translations.txt \
+        model.train_ds.src_file_name=${stage_train_data}/originals.txt \
+        model.train_ds.tgt_file_name=${stage_train_data}/translations.txt \
         model.machine_translation.tokenizer.tokenizer_model=${tok_model} \
         model.validation_ds.src_file_name=${valid_src} \
         model.validation_ds.tgt_file_name=${valid_ref} \
@@ -60,25 +55,24 @@ export stage1_dir=${result_dir}/stage1 \
         trainer.max_epochs=${max_epochs} \
         +trainer.max_steps=50000 \
         +exp_manager.resume_if_exists=${resume} \
-        +model.weights_checkpoint=${stage1_weights}
+        +model.weights_checkpoint=${stage1_weights} \
+        model.optim.lr=${back_translation_stages_lr}
   fi \
-  && export stage2_dir=${result_dir}/stage2 \
+  && stage2_dir=${result_dir}/stage2 \
   && mkdir -p ${stage2_dir} \
   && if [ ! -f ${stage2_dir}/best.ckpt ]; then
-    cat ${par_train_src} ${mono_data_path}/6M/translations.txt >"${stage2_dir}/originals.txt" \
-    && cat ${par_train_ref} ${mono_data_path}/6M/originals.txt >"${stage2_dir}/translations.txt" \
-    && python3 ${nemo_path}/utils/shuffle_pairs.py ${stage2_dir} \
     && if compgen -G ${stage2_dir}/TransformerMT/*/checkpoints/* > /dev/null; then
-      export resume=true
+      resume=true
     else
-      export resume=false
+      resume=false
     fi \
+    && stage_train_data="${data_path}/stage2" \
     && python3 train.py -cn ${base_conf} \
         trainer.num_nodes=${num_nodes} \
         trainer.gpus=${ngpus} \
         model.train_ds.tokens_in_batch=${train_n_tokens_in_batch} \
-        model.train_ds.src_file_name=${stage2_dir}/originals.txt \
-        model.train_ds.tgt_file_name=${stage2_dir}/translations.txt \
+        model.train_ds.src_file_name=${stage_train_data}/originals.txt \
+        model.train_ds.tgt_file_name=${stage_train_data}/translations.txt \
         model.machine_translation.tokenizer.tokenizer_model=${tok_model} \
         model.validation_ds.src_file_name=${valid_src} \
         model.validation_ds.tgt_file_name=${valid_ref} \
@@ -88,25 +82,24 @@ export stage1_dir=${result_dir}/stage1 \
         +exp_manager.resume_if_exists=${resume} \
         trainer.max_epochs=${max_epochs} \
         +trainer.max_steps=75000 \
-        +model.weights_checkpoint=${stage1_dir}/best.ckpt
+        +model.weights_checkpoint=${stage1_dir}/best.ckpt \
+        model.optim.lr=${back_translation_stages_lr}
   fi \
-  && export stage3_dir=${result_dir}/stage3 \
+  && stage3_dir=${result_dir}/stage3 \
   && mkdir -p ${stage3_dir} \
   && if [ ! -f ${stage3_dir}/best.ckpt ]; then
-    cat ${par_train_src} ${mono_data_path}/12M/translations.txt >"${stage3_dir}/originals.txt" \
-    && cat ${par_train_ref} ${mono_data_path}/12M/originals.txt >"${stage3_dir}/translations.txt" \
-    && python3 ${nemo_path}/utils/shuffle_pairs.py ${stage3_dir} \
     && if compgen -G ${stage3_dir}/TransformerMT/*/checkpoints/* > /dev/null; then
-      export resume=true
+      resume=true
     else
-      export resume=false
+      resume=false
     fi \
+    && stage_train_data="${data_path}/stage3" \
     && python3 train.py -cn ${base_conf} \
         trainer.num_nodes=${num_nodes} \
         trainer.gpus=${ngpus} \
         model.train_ds.tokens_in_batch=${train_n_tokens_in_batch} \
-        model.train_ds.src_file_name=${stage3_dir}/originals.txt \
-        model.train_ds.tgt_file_name=${stage3_dir}/translations.txt \
+        model.train_ds.src_file_name=${stage_train_data}/originals.txt \
+        model.train_ds.tgt_file_name=${stage_train_data}/translations.txt \
         model.machine_translation.tokenizer.tokenizer_model=${tok_model} \
         model.validation_ds.src_file_name=${valid_src} \
         model.validation_ds.tgt_file_name=${valid_ref} \
@@ -116,25 +109,24 @@ export stage1_dir=${result_dir}/stage1 \
         trainer.max_epochs=${max_epochs} \
         +trainer.max_steps=75000 \
         +exp_manager.resume_if_exists=${resume} \
-        +model.weights_checkpoint=${stage2_dir}/best.ckpt
+        +model.weights_checkpoint=${stage2_dir}/best.ckpt \
+        model.optim.lr=${back_translation_stages_lr}
   fi \
-  && export stage4_dir=${result_dir}/stage4 \
+  && stage4_dir=${result_dir}/stage4 \
   && mkdir -p ${stage4_dir} \
   && if [ ! -f ${stage4_dir}/best.ckpt ]; then
-    cat ${par_train_src} ${mono_data_path}/24M/translations.txt >"${stage4_dir}/originals.txt" \
-    && cat ${par_train_ref} ${mono_data_path}/24M/originals.txt >"${stage4_dir}/translations.txt" \
-    && python3 ${nemo_path}/utils/shuffle_pairs.py ${stage4_dir} \
     && if compgen -G ${stage4_dir}/TransformerMT/*/checkpoints/* > /dev/null; then
-      export resume=true
+      resume=true
     else
-      export resume=false
+      resume=false
     fi \
+    && stage_train_data="${data_path}/stage4" \
     && python3 train.py -cn ${base_conf} \
         trainer.num_nodes=${num_nodes} \
         trainer.gpus=${ngpus} \
         model.train_ds.tokens_in_batch=${train_n_tokens_in_batch} \
-        model.train_ds.src_file_name=${stage4_dir}/originals.txt \
-        model.train_ds.tgt_file_name=${stage4_dir}/translations.txt \
+        model.train_ds.src_file_name=${stage_train_data}/originals.txt \
+        model.train_ds.tgt_file_name=${stage_train_data}/translations.txt \
         model.machine_translation.tokenizer.tokenizer_model=${tok_model} \
         model.validation_ds.src_file_name=${valid_src} \
         model.validation_ds.tgt_file_name=${valid_ref} \
@@ -144,13 +136,14 @@ export stage1_dir=${result_dir}/stage1 \
         trainer.max_epochs=${max_epochs} \
         +trainer.max_steps=100000 \
         +exp_manager.resume_if_exists=${resume} \
-        +model.weights_checkpoint=${stage3_dir}/best.ckpt
+        +model.weights_checkpoint=${stage3_dir}/best.ckpt \
+        model.optim.lr=${back_translation_stages_lr}
   fi \
   && python3 test.py -cn ${base_conf} \
       trainer.num_nodes=${num_nodes} \
       trainer.gpus=${ngpus} \
-      model.train_ds.src_file_name=${stage4_dir}/originals.txt \
-      model.train_ds.tgt_file_name=${stage4_dir}/translations.txt \
+      model.train_ds.src_file_name=${stage_train_data}/originals.txt \
+      model.train_ds.tgt_file_name=${stage_train_data}/translations.txt \
       model.machine_translation.tokenizer.tokenizer_model=${tok_model} \
       model.validation_ds.src_file_name=${valid_src} \
       model.validation_ds.tgt_file_name=${valid_ref} \
--- examples/nlp/machine_translation/wmt18_with_back_translation/circe_commands/4_stages_post_traning/exports_en_de.sh
+++ examples/nlp/machine_translation/wmt18_with_back_translation/circe_commands/4_stages_post_traning/exports_en_de.sh
@@ -1,4 +1,4 @@
-export nemo_path=/code \
+export nemo_path=/workspace/NeMo \
   && echo "NeMo path: ${nemo_path}" \
   && export PYTHONPATH="${nemo_path}" \
   && cd  "${nemo_path}/examples/nlp/machine_translation" \
