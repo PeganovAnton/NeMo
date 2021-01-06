c357169f60590d77cf8da50ac2fefd064b2e24dd
PeganovAnton <peganoff2@mail.ru>
2021-01-06 16:21:37 +0300

    Fix transformers version
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/wmt18_with_back_translation/circe_commands/slurm_wrong/install/standard.sh
+++ examples/nlp/machine_translation/wmt18_with_back_translation/circe_commands/slurm_wrong/install/standard.sh
@@ -1,4 +1,3 @@
 pip3 install -r requirements/requirements.txt \
   && pip3 install -r requirements/requirements_nlp.txt \
-  && pip3 install webdataset \
-  && pip3 install transformers==3.5.0
+  && pip3 install webdataset
