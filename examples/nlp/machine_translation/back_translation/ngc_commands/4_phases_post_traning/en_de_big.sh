152150b5bc1b2ea383d92a8890afe561b204f00f
PeganovAnton <peganoff2@mail.ru>
2021-01-23 19:19:41 +0300

    backup
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/nlp/machine_translation/back_translation/ngc_commands/4_phases_post_traning/en_de_big.sh
@@ -0,0 +1,258 @@
+#!/bin/bash
+# The only argument is wandb token.
+
+WANDB_PROJECT=nmt_en_de_back_translation_4_phases_v0
+TRANSLATE_MODELS_WS=wmt_translate_models
+TRANSLATE_MODELS_PATH=/translate_models
+BACK_TRANSLATION_DS_ID=71127
+DATA_PATH=/data
+BACK_TRANSLATED_PATH=${DATA_PATH}/backtranslated
+
+read -r -d '' cmd <<EOF
+set -e -x
+git clone https://github.com/PeganovAnton/NeMo.git
+cd NeMo
+git checkout nmtmodel
+pip3 install -r requirements/requirements.txt
+pip3 install -r requirements/requirements_nlp.txt
+pip3 install --upgrade wandb
+pip3 install webdataset
+wandb login $1
+export nemo_path="\$(pwd)"
+export HYDRA_FULL_ERROR=1
+echo "NeMo path: \${nemo_path}"
+export PYTHONPATH="\${nemo_path}"
+cd  "\${nemo_path}/examples/nlp/machine_translation"
+num_gpus=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+
+
+base_conf=wmt16/en_de_8gpu
+train_n_tokens_in_batch=16000
+max_epochs=100000
+data_path=/data
+phase_data_path=${data_path}/4_stages_back_translation_en_de
+valid_src=${data_path}/parallel_clean/newstest2013-en-de.src
+valid_ref=${data_path}/parallel_clean/newstest2013-en-de.ref
+test_src=${data_path}/parallel_clean/newstest2014-en-de.src
+test_ref=${data_path}/parallel_clean/newstest2014-en-de.ref
+result_dir=/result
+tok_model=${data_path}/bpe_35k_en_de_yttm.model
+phase0_base_lr=0.0005
+back_translation_phases_base_lr=0.00005
+lr_num_nodes_factor=$(python -c "print(${SLURM_NUM_NODES}**0.5)")
+echo "Learning rate number of nodes factor ${lr_num_nodes_factor}"
+phase0_lr=$(python -c "print(${phase0_base_lr} * ${lr_num_nodes_factor})")
+back_translation_phases_lr=$(python -c "print(${back_translation_phases_base_lr} * ${lr_num_nodes_factor})")
+encoder_bpe_dropout=0.1
+decoder_bpe_dropout=0.1
+
+phase0_dir=${result_dir}/phase0
+phase0_train_data="${phase_data_path}/phase0"
+max_steps0=60000
+
+phase1_dir=${result_dir}/phase1
+phase1_train_data="${phase_data_path}/phase1"
+max_steps1=100000
+
+phase2_dir=${result_dir}/phase2
+phase2_train_data="${phase_data_path}/phase2"
+max_steps2=150000
+
+phase3_dir=${result_dir}/phase3
+phase3_train_data="${phase_data_path}/phase3"
+max_steps3=150000
+
+phase4_dir=${result_dir}/phase4
+phase4_train_data="${phase_data_path}/phase4"
+max_steps4=200000
+
+
+mkdir -p ${phase0_dir} \
+if [ ! -L ${phase0_dir}/best.ckpt ]; then
+  if compgen -G ${phase0_dir}/TransformerMT/checkpoints/* > /dev/null; then
+    echo "Will try to resume PHASE 0 training from checkpoint"
+    resume0=true
+  else
+    echo "Checkpoints for PHASE 0 are not found. Training will be started from scratch."
+    resume0=false
+  fi
+  python enc_dec_nmt.py --config-name=aayn_base \
+    trainer.gpus=${num_gpus} \
+    ~trainer.max_epochs \
+    +trainer.max_steps=${max_steps0}  \
+    model.encoder_tokenizer.tokenizer_model=${tok_model}  \
+    model.encoder_tokenizer.bpe_dropout=${encoder_bpe_dropout} \
+    model.decoder_tokenizer.tokenizer_model=${tok_model}  \
+    model.decoder_tokenizer.bpe_dropout=${decoder_bpe_dropout} \
+    model.train_ds.tokens_in_batch=${train_n_tokens_in_batch} \
+    model.train_ds.src_file_name=${phase0_train_data}/originals.txt \
+    model.train_ds.tgt_file_name=${phase0_train_data}/translations.txt \
+    model.validation_ds.src_file_name=${valid_src} \
+    model.validation_ds.tgt_file_name=${valid_ref} \
+    model.test_ds.src_file_name=${test_src} \
+    model.test_ds.tgt_file_name=${test_ref} \
+    model.optim.lr=${phase0_lr}  \
+    exp_manager.wandb_logger_kwargs.name=phase0 \
+    exp_manager.wandb_logger_kwargs.project=${WANDB_PROJECT} \
+    +exp_manager.exp_dir=${phase0_dir} \
+    +exp_manager.resume_if_exists=${resume0}
+  echo  "*******FINISHING 0TH PHASE********"
+fi
+
+mkdir -p ${phase1_dir} \
+if [ ! -L ${phase1_dir}/best.ckpt ]; then
+  if compgen -G ${phase1_dir}/TransformerMT/checkpoints/* > /dev/null; then
+    echo "Will try to resume PHASE 1 training from checkpoint"
+    resume1=true
+  else
+    echo "Checkpoints for PHASE 1 are not found. Training will be started from scratch."
+    resume1=false
+  fi
+  python enc_dec_nmt.py --config-name=aayn_base \
+    trainer.gpus=${num_gpus} \
+    ~trainer.max_epochs \
+    +trainer.max_steps=${max_steps1}  \
+    model.encoder_tokenizer.tokenizer_model=${tok_model}  \
+    model.encoder_tokenizer.bpe_dropout=${encoder_bpe_dropout} \
+    model.decoder_tokenizer.tokenizer_model=${tok_model}  \
+    model.decoder_tokenizer.bpe_dropout=${decoder_bpe_dropout} \
+    model.train_ds.tokens_in_batch=${train_n_tokens_in_batch} \
+    model.train_ds.src_file_name=${phase1_train_data}/originals.txt \
+    model.train_ds.tgt_file_name=${phase1_train_data}/translations.txt \
+    model.validation_ds.src_file_name=${valid_src} \
+    model.validation_ds.tgt_file_name=${valid_ref} \
+    model.test_ds.src_file_name=${test_src} \
+    model.test_ds.tgt_file_name=${test_ref} \
+    model.optim.lr=${phase1_lr}  \
+    exp_manager.wandb_logger_kwargs.name=phase1 \
+    exp_manager.wandb_logger_kwargs.project=${WANDB_PROJECT} \
+    +exp_manager.exp_dir=${phase1_dir} \
+    +exp_manager.resume_if_exists=${resume1}
+  echo  "*******FINISHING 1TH PHASE********"
+fi
+
+mkdir -p ${phase2_dir} \
+if [ ! -L ${phase2_dir}/best.ckpt ]; then
+  if compgen -G ${phase2_dir}/TransformerMT/checkpoints/* > /dev/null; then
+    echo "Will try to resume PHASE 2 training from checkpoint"
+    resume2=true
+  else
+    echo "Checkpoints for PHASE 2 are not found. Training will be started from scratch."
+    resume2=false
+  fi
+  python enc_dec_nmt.py --config-name=aayn_base \
+    trainer.gpus=${num_gpus} \
+    ~trainer.max_epochs \
+    +trainer.max_steps=${max_steps2}  \
+    model.encoder_tokenizer.tokenizer_model=${tok_model}  \
+    model.encoder_tokenizer.bpe_dropout=${encoder_bpe_dropout} \
+    model.decoder_tokenizer.tokenizer_model=${tok_model}  \
+    model.decoder_tokenizer.bpe_dropout=${decoder_bpe_dropout} \
+    model.train_ds.tokens_in_batch=${train_n_tokens_in_batch} \
+    model.train_ds.src_file_name=${phase2_train_data}/originals.txt \
+    model.train_ds.tgt_file_name=${phase2_train_data}/translations.txt \
+    model.validation_ds.src_file_name=${valid_src} \
+    model.validation_ds.tgt_file_name=${valid_ref} \
+    model.test_ds.src_file_name=${test_src} \
+    model.test_ds.tgt_file_name=${test_ref} \
+    model.optim.lr=${phase2_lr}  \
+    exp_manager.wandb_logger_kwargs.name=phase2 \
+    exp_manager.wandb_logger_kwargs.project=${WANDB_PROJECT} \
+    +exp_manager.exp_dir=${phase2_dir} \
+    +exp_manager.resume_if_exists=${resume2}
+  echo  "*******FINISHING 2TH PHASE********"
+fi
+
+mkdir -p ${phase3_dir} \
+if [ ! -L ${phase3_dir}/best.ckpt ]; then
+  if compgen -G ${phase3_dir}/TransformerMT/checkpoints/* > /dev/null; then
+    echo "Will try to resume PHASE 3 training from checkpoint"
+    resume3=true
+  else
+    echo "Checkpoints for PHASE 3 are not found. Training will be started from scratch."
+    resume3=false
+  fi
+  python enc_dec_nmt.py --config-name=aayn_base \
+    trainer.gpus=${num_gpus} \
+    ~trainer.max_epochs \
+    +trainer.max_steps=${max_steps3}  \
+    model.encoder_tokenizer.tokenizer_model=${tok_model}  \
+    model.encoder_tokenizer.bpe_dropout=${encoder_bpe_dropout} \
+    model.decoder_tokenizer.tokenizer_model=${tok_model}  \
+    model.decoder_tokenizer.bpe_dropout=${decoder_bpe_dropout} \
+    model.train_ds.tokens_in_batch=${train_n_tokens_in_batch} \
+    model.train_ds.src_file_name=${phase3_train_data}/originals.txt \
+    model.train_ds.tgt_file_name=${phase3_train_data}/translations.txt \
+    model.validation_ds.src_file_name=${valid_src} \
+    model.validation_ds.tgt_file_name=${valid_ref} \
+    model.test_ds.src_file_name=${test_src} \
+    model.test_ds.tgt_file_name=${test_ref} \
+    model.optim.lr=${phase3_lr}  \
+    exp_manager.wandb_logger_kwargs.name=phase3 \
+    exp_manager.wandb_logger_kwargs.project=${WANDB_PROJECT} \
+    +exp_manager.exp_dir=${phase3_dir} \
+    +exp_manager.resume_if_exists=${resume3}
+  echo  "*******FINISHING 3TH PHASE********"
+fi
+
+mkdir -p ${phase4_dir} \
+if [ ! -L ${phase4_dir}/best.ckpt ]; then
+  if compgen -G ${phase4_dir}/TransformerMT/checkpoints/* > /dev/null; then
+    echo "Will try to resume PHASE 4 training from checkpoint"
+    resume4=true
+  else
+    echo "Checkpoints for PHASE 4 are not found. Training will be started from scratch."
+    resume4=false
+  fi
+  python enc_dec_nmt.py --config-name=aayn_base \
+    trainer.gpus=${num_gpus} \
+    ~trainer.max_epochs \
+    +trainer.max_steps=${max_steps4}  \
+    model.encoder_tokenizer.tokenizer_model=${tok_model}  \
+    model.encoder_tokenizer.bpe_dropout=${encoder_bpe_dropout} \
+    model.decoder_tokenizer.tokenizer_model=${tok_model}  \
+    model.decoder_tokenizer.bpe_dropout=${decoder_bpe_dropout} \
+    model.train_ds.tokens_in_batch=${train_n_tokens_in_batch} \
+    model.train_ds.src_file_name=${phase4_train_data}/originals.txt \
+    model.train_ds.tgt_file_name=${phase4_train_data}/translations.txt \
+    model.validation_ds.src_file_name=${valid_src} \
+    model.validation_ds.tgt_file_name=${valid_ref} \
+    model.test_ds.src_file_name=${test_src} \
+    model.test_ds.tgt_file_name=${test_ref} \
+    model.optim.lr=${phase4_lr}  \
+    exp_manager.wandb_logger_kwargs.name=phase4 \
+    exp_manager.wandb_logger_kwargs.project=${WANDB_PROJECT} \
+    +exp_manager.exp_dir=${phase4_dir} \
+    +exp_manager.resume_if_exists=${resume4}
+  echo  "*******FINISHING 4TH PHASE********"
+fi
+
+wmt13_translated=/result/newstest2013_en_translated.txt
+python3 nmt_transformer_infer.py \
+  --model ${phase4_dir}/best.ckpt \
+  --srctext /data/wmt13_en_de.src \
+  --tgtout "\${wmt13_translated}" \
+  --target_lang de
+cat "\${wmt13_translated}" | sacrebleu -t wmt13 -l en-de
+wmt14_translated=/result/newstest2014_en_translated.txt
+python3 nmt_transformer_infer.py \
+  --model ${phase4_dir}/best.ckpt \
+  --srctext /data/wmt14_en_de.src \
+  --tgtout "\${wmt14_translated}" \
+  --target_lang de
+cat "\${wmt14_translated}" | sacrebleu -t wmt14 -l en-de
+EOF
+
+ngc batch run \
+  --name big_en_de_attention_is_all_you_need_close_to_paper \
+  --preempt RUNONCE \
+  --image nvidia/pytorch:20.09-py3 \
+  --ace nv-us-west-2 \
+  --instance dgx1v.32g.8.norm \
+  --commandline "${cmd}" \
+  --result /result \
+  --org nvidian \
+  --team ac-aiapps \
+  --datasetid ${BACK_TRANSLATION_DS_ID}:${BACK_TRANSLATED_PATH} \
+  --workspace ${TRANSLATE_MODELS}:${TRANSLATE_MODELS_PATH}
+
