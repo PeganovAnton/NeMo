71736bcf86721ee4111531a983489519e32e7c1f
PeganovAnton <peganoff2@mail.ru>
2021-01-25 12:41:38 +0300

    Add xlf preprocessing script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ utils/extract_data_from_xlf_format.py
@@ -0,0 +1,39 @@
+import argparse
+import re
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "input",
+        help="Path to input rapid tilde .xlf file.",
+        type=Path,
+    )
+    args = parser.parse_args()
+    args.input = args.input.expanduser()
+    return args
+
+
+def main():
+    args = get_args()
+    tree = ET.parse(args.input)
+    root = tree.getroot()
+    m = re.match(r'\{(.*)\}', root.tag)
+    namespaces = {"doc": m.group(1)}
+    src_lang = root.get("srcLang")
+    tgt_lang = root.get("tgtLang")
+    dir_ = args.input.parent
+    src_path = dir_ / Path(src_lang)
+    tgt_path = dir_ / Path(tgt_lang)
+    with src_path.open('w') as src_f, tgt_path.open('w') as tgt_f:
+        for segment in root.findall("./doc:file/doc:unit/doc:segment", namespaces):
+            src = segment.find("./doc:source", namespaces).text
+            tgt = segment.find("./doc:target", namespaces).text
+            src_f.write(src + '\n')
+            tgt_f.write(tgt + '\n')
+
+
+if __name__ == "__main__":
+    main()
