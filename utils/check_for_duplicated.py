929351e691e49178e79f1aa81adb5d1f4b00e127
PeganovAnton <peganoff2@mail.ru>
2021-01-25 17:09:00 +0300

    Update check duplicates script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- utils/check_for_duplicated.py
+++ utils/check_for_duplicated.py
@@ -15,12 +15,12 @@ def get_args():
         type=Path,
     )
     parser.add_argument(
-        "tgt1",
+        "--tgt1",
         help="The first file with targets",
         type=Path,
     )
     parser.add_argument(
-        "tgt2",
+        "--tgt2",
         help="The second file with targets",
         type=Path,
     )
@@ -34,13 +34,21 @@ def get_args():
 
 def main():
     args = get_args()
-    with args.src1.open() as src1, \
-            args.src2.open() as src2, \
-            args.tgt1.open() as tgt1, \
-            args.tgt2.open() as tgt2:
-        s1 = set(zip(src1.readlines(), tgt1.readlines()))
-        s2 = set(zip(src2.readlines(), tgt2.readlines()))
-    print(f"Number of duplicates between ({args.src1}, {args.tgt1}) and ({args.src2}, {args.tgt2}):", len(s1 & s2))
+    if args.tgt1 is not None or args.tgt2 is not None:
+        with args.src1.open() as src1, \
+                args.src2.open() as src2, \
+                args.tgt1.open() as tgt1, \
+                args.tgt2.open() as tgt2:
+            s1 = set(zip(src1.readlines(), tgt1.readlines()))
+            s2 = set(zip(src2.readlines(), tgt2.readlines()))
+            print(f"Number of duplicates between ({args.src1}, {args.tgt1}) and ({args.src2}, {args.tgt2}):",
+                  len(s1 & s2))
+    else:
+        with args.src1.open() as src1, \
+                args.src2.open() as src2:
+            s1 = set(src1.readlines())
+            s2 = set(src2.readlines())
+        print(f"Number of duplicates between {args.src1} and {args.src2}:", len(s1 & s2))
 
 
 if __name__ == "__main__":
