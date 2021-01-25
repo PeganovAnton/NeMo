63642c601c9f3be6fc534e2ecee570c2a8282a02
PeganovAnton <peganoff2@mail.ru>
2021-01-25 13:21:53 +0300

    Update duplication check script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- utils/check_for_duplicated.py
+++ utils/check_for_duplicated.py
@@ -1,26 +1,46 @@
 import argparse
+from pathlib import Path
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "file1",
-        help="The first file with sentences"
+        "src1",
+        help="The first file with sources",
+        type=Path,
     )
     parser.add_argument(
-        "file2",
-        help="The second file with sentences"
+        "src2",
+        help="The second file with sources",
+        type=Path,
     )
-    return parser.parse_args()
-
+    parser.add_argument(
+        "tgt1",
+        help="The first file with targets",
+        type=Path,
+    )
+    parser.add_argument(
+        "tgt2",
+        help="The second file with targets",
+        type=Path,
+    )
+    args = parser.parse_args()
+    args.src1 = args.src1.expanduser()
+    args.src2 = args.src2.expanduser()
+    args.tgt1 = args.tgt1.expanduser()
+    args.tgt2 = args.tgt2.expanduser()
+    return args
 
 
 def main():
     args = get_args()
-    with open(args.file1) as f1, open(args.file2) as f2:
-        s1 = set(f1.readlines())
-        s2 = set(f2.readlines())
-    print("Number of duplicates:", len(s1 & s2))
+    with args.src1.open() as src1, \
+            args.src2.open() as src2, \
+            args.tgt1.open() as tgt1, \
+            args.tgt2.open() as tgt2:
+        s1 = set(zip(src1.readlines(), tgt1.readlines()))
+        s2 = set(zip(src2.readlines(), tgt2.readlines()))
+    print(f"Number of duplicates between ({args.src1}, {args.tgt1}) and ({args.src2} and {args.tgt2}):", len(s1 & s2))
 
 
 if __name__ == "__main__":
