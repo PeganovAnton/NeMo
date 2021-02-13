7284cda03b7f446b12fa0bbeb97fc0e6ac3fb6a8
PeganovAnton <peganoff2@mail.ru>
2021-02-13 17:22:44 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- utils/alphabetical_check.py
+++ utils/alphabetical_check.py
@@ -33,6 +33,19 @@ def get_args():
         help="Path to the output target file."
     )
     parser.add_argument(
+        "--removed_src",
+        "-r",
+        required=True,
+        type=Path,
+        help="Path to a file where removed source lines will be put."
+    )
+    parser.add_argument(
+        "--removed_tgt",
+        "-R",
+        type=Path,
+        help="Path to a file where removed target lines will be put."
+    )
+    parser.add_argument(
         "--src_lang",
         "-l",
         required=True,
@@ -48,27 +61,31 @@ def get_args():
     args = parser.parse_args()
     args.input_src = args.input_src.expanduser()
     args.output_src = args.output_src.expanduser()
+    args.removed_input = args.removed_input.expanduser()
     if args.input_target is not None:
         args.input_target = args.input_target.expanduser()
         args.output_target = args.output_target.expanduser()
+        args.removed_target = args.removed_target.expanduser()
         if args.tgt_lang is None:
             raise ValueError("If input target is provided than target language should be provided.")
     return args
 
 
-def filter_singles(input, output, lang):
+def filter_singles(input, output, removed, lang):
     alphabet = set(hg.Languages.get_alphabet([lang]))
     out_dir = output.parent
     out_dir.mkdir(parents=True, exist_ok=True)
     tmp_file = out_dir / Path('.tmp')
-    with tmp_file.open('w') as out_f, input.open() as in_f:
+    with tmp_file.open('w') as out_f, input.open() as in_f, removed.open('w') as r_f:
         for l in in_f:
             if set(l) & alphabet:
                 out_f.write(l)
+            else:
+                r_f.write(l)
     tmp_file.rename(output)
 
 
-def filter_pairs(input_src, input_tgt, output_src, output_tgt, src_lang, tgt_lang):
+def filter_pairs(input_src, input_tgt, output_src, output_tgt, removed_src, removed_tgt, src_lang, tgt_lang):
     src_alph = set(hg.Languages.get_alphabet([src_lang]))
     tgt_alph = set(hg.Languages.get_alphabet([tgt_lang]))
     out_src_dir = output_src.parent
@@ -78,11 +95,14 @@ def filter_pairs(input_src, input_tgt, output_src, output_tgt, src_lang, tgt_lan
     tmp_src = out_src_dir / Path('.tmp_src')
     tmp_tgt = out_tgt_dir / Path('.tmp_tgt')
     with tmp_src.open('w') as out_s_f, tmp_tgt.open('w') as out_t_f, input_src.open() as in_s_f, \
-            input_tgt.open('w') as in_t_f:
+            input_tgt.open() as in_t_f, removed_src.open('w') as r_s_f, removed_tgt.open('w') as r_t_f:
         for s_l, t_l in zip(in_s_f, in_t_f):
             if set(s_l) & src_alph and set(t_l) & tgt_alph:
                 out_s_f.write(s_l)
                 out_t_f.write(t_l)
+            else:
+                r_s_f.write(s_l)
+                r_t_f.write(t_l)
     tmp_src.rename(output_src)
     tmp_tgt.rename(output_tgt)
 
@@ -90,9 +110,17 @@ def filter_pairs(input_src, input_tgt, output_src, output_tgt, src_lang, tgt_lan
 def main():
     args = get_args()
     if args.input_target is None:
-        filter_singles(args.input_src, args.output_src, args.src_lang)
+        filter_singles(args.input_src, args.output_src, args.removed_src, args.src_lang)
     else:
-        filter_pairs(args.input_src, args.input_tgt, args.output_src, args.output_tgt, args.src_lang, args.tgt_lang)
+        filter_pairs(
+            args.input_src,
+            args.input_tgt,
+            args.output_src,
+            args.output_tgt,
+            args.removed_src,
+            args.removed_tgt,
+            args.src_lang,
+            args.tgt_lang)
 
 
 if __name__ == "__main__":
