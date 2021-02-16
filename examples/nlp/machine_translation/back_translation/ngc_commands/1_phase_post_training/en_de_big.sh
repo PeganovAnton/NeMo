60dabaee458aec9ed4c079263c119122f7e7051a
PeganovAnton <peganoff2@mail.ru>
2021-02-16 16:12:43 +0300

    Add first working variant of ngc 1 phase script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/back_translation/ngc_commands/1_phase_post_training/en_de_big.sh
+++ examples/nlp/machine_translation/back_translation/ngc_commands/1_phase_post_training/en_de_big.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 # The only argument is wandb token.
 
-WANDB_PROJECT=nmt_en_de_back_translation_1_phases_v0
-TRANSLATE_MODELS_WS=wmt_translate_models
+WANDB_PROJECT=1_phase_post_training_on_sandeep_back_translated_data
+TRANSLATE_MODELS_WS=translation_pretrained_weights
 TRANSLATE_MODELS_PATH=/wmt_translate_models
-BACK_TRANSLATION_DS_ID=71127
+BACK_TRANSLATION_DS_ID=74337
 DATA_PATH=/data
+
+TRAIN_N_TOKENS_IN_BATCH=16000
+MAX_EPOCHS=100000
+MAX_STEPS=100000
+DATA_PATH=${DATA}/text
+TRAIN_SRC=${DATA_PATH}/train.en
+TRAIN_REF=${DATA_PATH}/train.de
+VALID_SRC=${DATA_PATH}/newstest2013.en
+VALID_REF=${DATA_PATH}/newstest2013.de
+TEST_SRC=${DATA_PATH}/newstest2014.en
+TEST_REF=${DATA_PATH}/newstest2014.de
+RESULT_DIR=/result
+PRETRAINED_PATH=${TRANSLATE_MODELS_PATH}/en_de_large
+TOK_MODEL=${PRETRAINED_PATH}/tokenizer.latest.60.32000.BPE.model
+BASE_LR=0.0005
+ENCODER_BPE_DROPOUT=0.1
+DECODER_BPE_DROPOUT=0.1
+EXP_DIR=${RESULT_DIR}/exp_dir
+
+WMT13_TRANSLATED=${RESULT_DIR}/newstest2013_en_translated.txt
+WMT14_TRANSLATED=${RESULT_DIR}/newstest2014_en_translated.txt
 
 read -r -d '' cmd <<EOF
 set -e -x
@@ -21,65 +42,50 @@ export nemo_path="\$(pwd)"
 export HYDRA_FULL_ERROR=1
 echo "NeMo path: \${nemo_path}"
 export PYTHONPATH="\${nemo_path}"
-cd  "\${nemo_path}/examples/nlp/machine_translation"
-num_gpus=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+cd "\${nemo_path}/examples/nlp/machine_translation"
+num_gpus=\$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 
-train_n_tokens_in_batch=16000
-max_epochs=100000
-data_path=/data
-phase_data_path=${data_path}/4_stages_back_translation_en_de
-valid_src=${data_path}/parallel_clean/newstest2013-en-de.src
-valid_ref=${data_path}/parallel_clean/newstest2013-en-de.ref
-test_src=${data_path}/parallel_clean/newstest2014-en-de.src
-test_ref=${data_path}/parallel_clean/newstest2014-en-de.ref
-result_dir=/result
-tok_model=${data_path}/bpe_35k_en_de_yttm.model
-phase0_base_lr=0.0005
-back_translation_phases_base_lr=0.00005
-back_translation_phases_lr=$(python -c "print(${back_translation_phases_base_lr})")
-encoder_bpe_dropout=0.1
-decoder_bpe_dropout=0.1
+
 python train.py --config-name=aayn_big \
-  trainer.gpus=${num_gpus} \
-  ~trainer.max_epochs \
-  +trainer.max_steps=${max_steps1}  \
-  model.encoder_tokenizer.tokenizer_model=${tok_model}  \
-  model.encoder_tokenizer.bpe_dropout=${encoder_bpe_dropout} \
-  model.decoder_tokenizer.tokenizer_model=${tok_model}  \
-  model.decoder_tokenizer.bpe_dropout=${decoder_bpe_dropout} \
-  model.train_ds.tokens_in_batch=${train_n_tokens_in_batch} \
-  model.train_ds.src_file_name=${phase1_train_data}/originals.txt \
-  model.train_ds.tgt_file_name=${phase1_train_data}/translations.txt \
-  model.validation_ds.src_file_name=${valid_src} \
-  model.validation_ds.tgt_file_name=${valid_ref} \
-  model.test_ds.src_file_name=${test_src} \
-  model.test_ds.tgt_file_name=${test_ref} \
-  model.optim.lr=${phase1_lr}  \
-  exp_manager.wandb_logger_kwargs.name=phase1 \
+  trainer.gpus=\${num_gpus} \
+  ~trainer.MAX_EPOCHS \
+  +trainer.max_steps=${MAX_STEPS}  \
+  +model.weights_checkpoin=${PRETRAINED_PATH}/model_weights.ckpt \
+  model.encoder_tokenizer.tokenizer_model=${TOK_MODEL}  \
+  model.encoder_tokenizer.bpe_dropout=${ENCODER_BPE_DROPOUT} \
+  model.decoder_tokenizer.tokenizer_model=${TOK_MODEL}  \
+  model.decoder_tokenizer.bpe_dropout=${DECODER_BPE_DROPOUT} \
+  model.train_ds.tokens_in_batch=${TRAIN_N_TOKENS_IN_BATCH} \
+  model.train_ds.src_file_name=${TRAIN_SRC} \
+  model.train_ds.tgt_file_name=${TRAIN_REF} \
+  model.validation_ds.src_file_name=${VALID_SRC} \
+  model.validation_ds.tgt_file_name=${VALID_REF} \
+  model.test_ds.src_file_name=${TEST_SRC} \
+  model.test_ds.tgt_file_name=${TEST_REF} \
+  model.optim.lr=${BASE_LR}  \
+  exp_manager.wandb_logger_kwargs.name=1st_trial \
   exp_manager.wandb_logger_kwargs.project=${WANDB_PROJECT} \
-  +exp_manager.exp_dir=${phase1_dir} \
-  +exp_manager.resume_if_exists=${resume1}
+  +exp_manager.exp_dir=${EXP_DIR}
 
-wmt13_translated=/result/newstest2013_en_translated.txt
 python3 nmt_transformer_infer.py \
   --model ${phase1_dir}/best.ckpt \
   --srctext /data/wmt13_en_de.src \
-  --tgtout "\${wmt13_translated}" \
+  --tgtout "${WMT13_TRANSLATED}" \
   --target_lang de
-cat "\${wmt13_translated}" | sacrebleu -t wmt13 -l en-de
-wmt14_translated=/result/newstest2014_en_translated.txt
+cat "${WMT13_TRANSLATED}" | sacrebleu -t wmt13 -l en-de
+
 python3 nmt_transformer_infer.py \
   --model ${phase1_dir}/best.ckpt \
   --srctext /data/wmt14_en_de.src \
-  --tgtout "\${wmt14_translated}" \
+  --tgtout "${WMT14_TRANSLATED}" \
   --target_lang de
-cat "\${wmt14_translated}" | sacrebleu -t wmt14 -l en-de
+cat "${WMT14_TRANSLATED}" | sacrebleu -t wmt14 -l en-de
 EOF
 
 ngc batch run \
   --name big_en_de_attention_is_all_you_need_close_to_paper \
   --preempt RUNONCE \
-  --image nvidia/pytorch:20.09-py3 \
+  --image nvidia/pytorch:20.11-py3 \
   --ace nv-us-west-2 \
   --instance dgx1v.32g.8.norm \
   --commandline "${cmd}" \
@@ -87,5 +93,5 @@ ngc batch run \
   --org nvidian \
   --team ac-aiapps \
   --datasetid ${BACK_TRANSLATION_DS_ID}:${BACK_TRANSLATED_PATH} \
-  --workspace ${TRANSLATE_MODELS_WS}:${TRANSLATE_MODELS_PATH}
+  --workspace ${TRANSLATE_MODELS_WS}:${TRANSLATE_MODELS_PATH}:RO
 
