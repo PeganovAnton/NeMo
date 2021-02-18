8c19a002356f8ffe8d3faaa2974609831e14fa82
PeganovAnton <peganoff2@mail.ru>
2021-02-18 19:53:32 +0300

    Add slurm translate script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/back_translation/translation/slurm.sh
+++ examples/nlp/machine_translation/back_translation/translation/slurm.sh
@@ -16,9 +16,19 @@
 # Hyper params
 # Env Variable Setup
 
+# You have to provide following variables:
+# DATA_DIR -- path to tar files
+# WANDB_TOKEN
+# MODEL -- the name of a model from rc1 folder
+# TAR_TEMPLATE -- template of tar files (see tarred dataset documentation)
+# SOURCE_LANG
+# TARGET_LANG
+
+
 WANDB_PROJECT=back_translation_de_en
 CONTAINER="rumpelschtilzchen/pytorch-20.11-py3-nemo-nmt:latest"
-DATA_DIR='/gpfs/fs1/apeganov/datasets/news_en_de_mono'
+# Pass DATA_DIR as a parameter
+data_dir=/data
 RESULT_DIR='/home/apeganov'
 result_dir=/result
 PRETRAINED_MODELS='/gpfs/fs1/apeganov/models/mt/wmt_translate_models/rc1'
@@ -30,107 +40,31 @@ export PYTHONFAULTHANDLER=1
 mkdir -p $RESULT_DIR/$EXP_NAME
 MOUNTS="--container-mounts=$DATA_DIR:/data,$RESULT_DIR/$EXP_NAME:${result_dir}:${PRETRAINED_MODELS}:${pretrained_models}"
 
+tar_files=${data_dir}/${TAR_TEMPLATE}
+metadata=${data_dir}/metadata.json
 
-TRANSLATE_MODELS_WS=trainslation_pretrained_weights
-TRANSLATE_MODELS_PATH=/wmt_translate_models
-DS_ID=74337
-DATA_PATH=/data
-
-TRAIN_N_TOKENS_IN_BATCH=16000
-MAX_EPOCHS=100000
-MAX_STEPS=100000
-TEXT_PATH=${DATA_PATH}/text
-TARRED_PATH=${DATA_PATH}/tarred
-RAID=/raid
-RAID_TRAIN_PATH=${RAID}/train
-TRAIN_SRC=${RAID_TRAIN_PATH}/batches.tokens.16000._OP_1..71_CL_.tar
-TRAIN_REF=${RAID_TRAIN_PATH}/batches.tokens.16000._OP_1..71_CL_.tar
-TRAIN_METADATA=${RAID_TRAIN_PATH}/metadata.json
-VALID_SRC=${RAID}/newstest2013.en
-VALID_REF=${RAID}/newstest2013.de
-TEST_SRC=${RAID}/newstest2014.en
-TEST_REF=${RAID}/newstest2014.de
-RESULT_DIR=/result
-PRETRAINED_PATH=${TRANSLATE_MODELS_PATH}/large_en_de
-TOK_MODEL=${PRETRAINED_PATH}/tokenizer.latest.60.32000.BPE.model
-BASE_LR=0.0005
-ENCODER_BPE_DROPOUT=0.1
-DECODER_BPE_DROPOUT=0.1
-EXP_DIR=${RESULT_DIR}/exp_dir
-
-WMT13_TRANSLATED=${RESULT_DIR}/newstest2013_en_translated.txt
-WMT14_TRANSLATED=${RESULT_DIR}/newstest2014_en_translated.txt
+OUTFILE="${RESULT_DIR}/${EXP_NAME}/slurm-%j-%n.out"
+ERRFILE="${RESULT_DIR}/${EXP_NAME}/error-%j-%n.out"
 
 read -r -d '' cmd <<EOF
-set -e -x
-git clone https://github.com/PeganovAnton/NeMo.git
-cd NeMo
-git checkout sacreBLEU_pl_metric
-pip3 install -r requirements/requirements.txt
-pip3 install -r requirements/requirements_nlp.txt
-pip3 install --upgrade wandb
-pip3 install webdataset
-wandb login $1
-export nemo_path="\$(pwd)"
-export HYDRA_FULL_ERROR=1
+if [ "\${SLURM_PROCID}" -eq "0" ]; then wandb login ${WANDB_TOKEN}; fi
+nemo_path="\$(pwd)"
 echo "NeMo path: \${nemo_path}"
 export PYTHONPATH="\${nemo_path}"
 cd "\${nemo_path}/examples/nlp/machine_translation"
-num_gpus=\$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
-cp -rv ${TEXT_PATH}/newstest* ${RAID}/
-mkdir -p ${RAID_TRAIN_PATH}
-cp -rv ${TARRED_PATH}/* ${RAID_TRAIN_PATH}/
 
-python train.py --config-name=aayn_big \
-  trainer.gpus=\${num_gpus} \
-  ~trainer.max_epochs \
-  +trainer.max_steps=${MAX_STEPS}  \
-  +model.weights_checkpoint=${PRETRAINED_PATH}/model_weights.ckpt \
-  model.encoder_tokenizer.tokenizer_model=${TOK_MODEL}  \
-  model.encoder_tokenizer.bpe_dropout=${ENCODER_BPE_DROPOUT} \
-  model.decoder_tokenizer.tokenizer_model=${TOK_MODEL}  \
-  model.decoder_tokenizer.bpe_dropout=${DECODER_BPE_DROPOUT} \
-  model.train_ds.tokens_in_batch=${TRAIN_N_TOKENS_IN_BATCH} \
-  model.train_ds.src_file_name=${TRAIN_SRC} \
-  model.train_ds.tgt_file_name=${TRAIN_REF} \
-  +model.train_ds.load_from_tarred_dataset=true \
-  +model.train_ds.metadata_path=${TRAIN_METADATA} \
-  model.validation_ds.src_file_name=${VALID_SRC} \
-  model.validation_ds.tgt_file_name=${VALID_REF} \
-  model.test_ds.src_file_name=${TEST_SRC} \
-  model.test_ds.tgt_file_name=${TEST_REF} \
-  model.optim.lr=${BASE_LR}  \
-  +model.find_unused_parameters=true \
-  exp_manager.wandb_logger_kwargs.name=1st_trial \
-  exp_manager.wandb_logger_kwargs.project=${WANDB_PROJECT} \
-  +exp_manager.exp_dir=${EXP_DIR}
-
-python3 nmt_transformer_infer.py \
-  --model ${phase1_dir}/best.ckpt \
-  --srctext /data/wmt13_en_de.src \
-  --tgtout "${WMT13_TRANSLATED}" \
-  --target_lang de
-cat "${WMT13_TRANSLATED}" | sacrebleu -t wmt13 -l en-de
-
-python3 nmt_transformer_infer.py \
-  --model ${phase1_dir}/best.ckpt \
-  --srctext /data/wmt14_en_de.src \
-  --tgtout "${WMT14_TRANSLATED}" \
-  --target_lang de
-cat "${WMT14_TRANSLATED}" | sacrebleu -t wmt14 -l en-de
+python translate_ddp.py \
+  --model=${pretrained_models}/${MODEL} \
+  --text2translate=${tar_files} \
+  --metadata_path ${metadata} \
+  --result_dir ${result_dir} \
+  --source_lang ${SOURCE_LANG} \
+  --target_lang ${TARGET_LANG}
 set +e +x
 EOF
 
-ngc batch run \
-  --name big_en_de_attention_is_all_you_need_close_to_paper \
-  --preempt RUNONCE \
-  --image nvidia/pytorch:20.11-py3 \
-  --ace nv-us-west-2 \
-  --instance dgx1v.16g.8.norm \
-  --commandline "${cmd}" \
-  --result /result \
-  --org nvidian \
-  --team ac-aiapps \
-  --datasetid ${DS_ID}:${DATA_PATH} \
-  --workspace ${TRANSLATE_MODELS_WS}:${TRANSLATE_MODELS_PATH}:RO
+srun -o $OUTFILE -e $ERRFILE \
+  --container-image="$CONTAINER" \
+  $MOUNTS \
+  bash -c "${cmd}"
 
