ae4b1df3970f6376a6bfa5a97875888139133305
PeganovAnton <peganoff2@mail.ru>
2021-02-18 11:35:08 -0800

    Minor fix
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/back_translation/translation/slurm.sh
+++ examples/nlp/machine_translation/back_translation/translation/slurm.sh
@@ -49,6 +49,7 @@ ERRFILE="${RESULT_DIR}/${EXP_NAME}/error-%j-%n.out"
 read -r -d '' cmd <<EOF
 set -e -x
 if [ "\${SLURM_PROCID}" -eq "0" ]; then wandb login ${WANDB_TOKEN}; fi
+cd NeMo
 nemo_path="\$(pwd)"
 echo "NeMo path: \${nemo_path}"
 export PYTHONPATH="\${nemo_path}"
